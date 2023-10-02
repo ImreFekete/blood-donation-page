40be41ed31dc34138c5a37fe835e6ab4c24d0a76
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2023-10-02 14:51:30 +0200

    Create backend skeleton with Spring Initializer

---
+++ backend/src/main/java/com/codecool/imf/ImfApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.imf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImfApplication.class, args);
+	}
+
+}
