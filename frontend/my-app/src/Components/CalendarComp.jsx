1a4ace9bcf9f1f924b6939d761d660b8bc63b9b6
BerryBusiness <120187639+BerryBusiness@users.noreply.github.com>
2023-10-03 14:24:32 +0200

    Add Calendar and Time components

---
+++ frontend/my-app/src/Components/CalendarComp.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import Time from "./Time.jsx";
+
+const CalendarComp = () => {
+
+    const [date, setDate] = useState(new Date())
+    const [showTime, setShowTime] = useState(false)
+    return (
+        <div className="calendar">
+            <h1 className="header">IMF Blood Calendar</h1>
+            <div>
+                <Calendar onChange={setDate} value={date} onClickDay={() => setShowTime(true)}/>
+            </div>
+            {date.length > 0 ? (
+                <p>
+                    <span>Start:</span>
+                    {date[0].toDateString()}
+                    &nbsp;
+                    &nbsp;
+                    <span>End:</span>{date[1].toDateString()}
+                </p>
+            ) : (
+                <p>
+                    <span>Default selected date:</span>{date.toDateString()}
+                </p>
+            )
+            }
+            <Time showTime={showTime} date={date}/>
+        </div>
+    );
+};
+
+export default CalendarComp;
