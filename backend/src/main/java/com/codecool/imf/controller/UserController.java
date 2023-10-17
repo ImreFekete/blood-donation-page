f0b1d199dc23dbfc953db97c99eb53d65d2b3473
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2023-10-17 15:25:43 +0200

    Modify user login backend controller

--- backend/src/main/java/com/codecool/imf/controller/UserController.java
+++ backend/src/main/java/com/codecool/imf/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.codecool.imf.controller;
 
+import com.codecool.imf.dto.AppointmentDTO;
 import com.codecool.imf.dto.UserDTO;
+import com.codecool.imf.model.Appointment;
 import com.codecool.imf.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +19,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public UserDTO getUserByEmail(@RequestBody UserDTO user) {
-        System.out.println(user);
-        return null;
+    public AppointmentDTO getUserAppointmentByEmail(@RequestBody UserDTO user) {
+        return userService.getUserAppointmentByEmail(user.getEmail());
     }
 }
