d4ace6c28dd4b3684e8f65da1e5dd320ea621f92
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2023-10-18 14:25:49 +0200

    Create RegisterPage on frontend

--- frontend/my-app/src/Pages/RegisterPage.jsx
+++ frontend/my-app/src/Pages/RegisterPage.jsx
@@ -1,11 +1,42 @@
-import LoginForm from "../Components/LoginForm.jsx";
-import {useNavigate} from "react-router-dom";
 import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import UserForm from "../Components/UserForm.jsx";
+import Loading from "../Components/Loading/index.js";
+
+const createUser = (user) => {
+    return fetch("/api/users/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+    }).then((res) => res.json());
+};
 
 const RegisterPage = () => {
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+
+    const handleCreateUser = (user) => {
+        setLoading(true);
+
+        createUser(user)
+            .then(() => {
+                    setLoading(false);
+                    navigate("/");
+                })
+    };
+
+    if (loading) {
+        return <Loading/>;
+    }
 
     return (
-        <p></p>
+        <UserForm
+            onSave={handleCreateUser}
+            disabled={loading}
+            onCancel={() => navigate("/")}
+        />
     )
 }
 
