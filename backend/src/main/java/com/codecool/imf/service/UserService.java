80e752abe07fe84d6eb61a36c0d1b68ec65a5644
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2023-10-19 14:24:21 +0200

    Create delete method in UserService

--- backend/src/main/java/com/codecool/imf/service/UserService.java
+++ backend/src/main/java/com/codecool/imf/service/UserService.java
@@ -8,10 +8,13 @@ import com.codecool.imf.model.Appointment;
 import com.codecool.imf.model.User;
 import com.codecool.imf.repository.AppointmentRepository;
 import com.codecool.imf.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -116,5 +119,22 @@ public class UserService {
             return true;
         }
         return false;
+    }
+
+    public boolean deleteUserById(Long id) {
+        try {
+            Optional<Appointment> optionalAppointment = appointmentRepository.findByUserId(id);
+            if (optionalAppointment.isPresent()) {
+                Appointment appointment = optionalAppointment.get();
+                System.out.println("SERVICE APPOINTMENT DELETE: " + appointment);
+                appointmentRepository.delete(appointment);
+            }
+            userRepository.deleteById(id);
+            System.out.println("TRUE");
+            return true;
+        } catch (EntityNotFoundException e) {
+            log.error(e.toString());
+            return false;
+        }
     }
 }
